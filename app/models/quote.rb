9186d0d4e39da4f086a0175c701393282cf7cefc
Nick Kokkos <nkokkos@gmail.com>
2022-11-15 15:20:10 +0200

    up to but not turbo streams and security

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,17 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  #after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote",
+  #                                               locals: {quote: self}, target: "quotes" }
+  #the above line can be replaced by:
+  #after_create_commit -> { broadcast_prepend_to "quotes" }
+  
+  #after_update_commit ->  { broadcast_replace_to "quotes" }
+  #after_destroy_commit -> { broadcast_remove_to  "quotes" }
+
+  # broadcasting asynchronous with activejob
+  after_create_commit  ->  { broadcast_prepend_to "quotes" }
+  after_update_commit  ->  { broadcast_replace_to "quotes" }
+  after_destroy_commit ->  { broadcast_remove_to  "quotes" }
 end
